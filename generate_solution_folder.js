85a1899379a533848c373a9834d0ffeec76d275c
Anatoli Makarevich <makaroni4@gmail.com>
2017-12-03 18:12:52 +0100

    Add solution template generator

---
+++ generate_solution_folder.js
@@ -0,0 +1,48 @@
+let fs = require('fs'),
+    folder = process.argv[2];
+
+if (!folder) {
+  throw 'Provide a folder name';
+}
+
+if (!fs.existsSync(folder)){
+  fs.mkdirSync(folder);
+}
+
+let solutionTemplate = `let fs = require('fs'),
+    expect = require('chai').expect;
+
+let solve = (input) => {
+  // solution
+};
+
+expect(solve("")).to.equal();
+
+let input = fs.readFileSync("input.dat").toString().trim();
+
+console.log(solve(input));
+`;
+
+fs.writeFile(`${folder}/input.dat`, "PUT INPUT DATA HERE", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`${folder}/input.dat was created.`);
+});
+
+fs.writeFile(`${folder}/solution_1.js`, solutionTemplate, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`${folder}/solution_1.js was created.`);
+});
+
+fs.writeFile(`${folder}/solution_2.js`, solutionTemplate, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`${folder}/solution_2.js was created.`);
+});
