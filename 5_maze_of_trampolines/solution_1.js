33b734967648da58190f0c632509dd66e2319251
Anatoli Makarevich <makaroni4@gmail.com>
2017-12-06 23:19:55 +0100

    Add solution for 1st part of 5th problem

---
+++ 5_maze_of_trampolines/solution_1.js
@@ -0,0 +1,26 @@
+let fs = require('fs'),
+    expect = require('chai').expect;
+
+let solve = (input) => {
+  let currentIndex = 0,
+      currentIteration = 0;
+
+  while(currentIndex <= input.length) {
+    let prevIndex = currentIndex;
+
+    if(input[currentIndex] !== 0) {
+      currentIndex += input[currentIndex];
+    }
+
+    input[prevIndex] += 1;
+    currentIteration += 1;
+  }
+
+  return currentIteration - 1;
+};
+
+expect(solve([0, 3, 0, 1, -3])).to.equal(5);
+
+let input = fs.readFileSync("input.dat").toString().trim().split("\n").map(x => parseInt(x, 10));
+
+console.log(solve(input));
