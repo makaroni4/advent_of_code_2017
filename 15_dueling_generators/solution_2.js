f8bb6ad918f0ca25b8980221e2215310a8d7f2c8
Anatoli Makarevich <makaroni4@gmail.com>
2018-05-05 00:08:09 +0200

    Add solution for 2nd part of 15th task

--- 15_dueling_generators/solution_2.js
+++ 15_dueling_generators/solution_2.js
@@ -1,12 +1,47 @@
-let fs = require('fs'),
-    expect = require('chai').expect;
+let fs = require("fs"),
+  expect = require("chai").expect;
 
-let solve = (input) => {
-  // solution
+class Generator {
+  constructor(factor, startingValue, multiplier) {
+    const LOWEST_16_BITS = 65535;
+
+    let value = startingValue;
+
+    this.nextValue = () => {
+      value = (factor * value) % 2147483647;
+
+      while(value % multiplier !== 0) {
+        value = (factor * value) % 2147483647;
+      }
+
+      return value & LOWEST_16_BITS;
+    };
+  }
+}
+
+(() => {
+  let generatorA = new Generator(16807, 65, 4);
+  expect(generatorA.nextValue().toString(2)).to.equal("1001100000100100");
+
+  let generatorB = new Generator(48271, 8921, 8);
+  expect(generatorB.nextValue().toString(2)).to.equal("1000010110001000");
+})();
+
+let solve = (startValueA, startValueB, totalPairs, multiplierA, multiplierB) => {
+  let generatorA = new Generator(16807, startValueA, multiplierA);
+  let generatorB = new Generator(48271, startValueB, multiplierB);
+  let matchCount = 0;
+
+  for (let i = totalPairs; i--;) {
+    if(generatorA.nextValue() === generatorB.nextValue()) {
+      matchCount += 1;
+    }
+  }
+
+  return matchCount;
 };
 
-expect(solve("")).to.equal();
+expect(solve(65, 8921, 1056, 4, 8)).to.equal(1);
+expect(solve(65, 8921, 5000000, 4, 8)).to.equal(309);
 
-let input = fs.readFileSync("input.dat").toString().trim();
-
-console.log(solve(input));
+console.log(solve(591, 393, 5000000, 4, 8));
